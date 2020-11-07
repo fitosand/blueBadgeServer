a9e87687d88889cb90ee307f3f9cf5170234e753
Jonathan Sieg <jonathanrsieg@gmail.com>
2020-11-07 16:07:34 -0500

    controllers and models added

---
+++ controllers/pointcontroller.js
@@ -0,0 +1,37 @@
+const router = require('express').Router();
+const Log = require('../db').import('../models/points');
+
+const validateSession = require('../middleware/validate-session');
+
+router.get('/', (req, res) => {
+    Log.findAll()
+    .then(logs => res.status(200).json(logs))
+    .catch(err => res.status(500).json({
+        error: err
+    }));
+});
+
+router.post('/', validateSession, (req,res) => {
+    const logFromRequest = {
+        typeOfPoint: req.body.typeOfPoint,
+        numberOfPoints: req.body.numberOfPoints,
+    }
+
+    Log.create(logFromRequest)
+        .then(log => res.status(200).json(log))
+        .catch(err => res.status(500).json(err))
+})
+
+router.get('/:name', (req, res) => {
+    Log.findOne({ where: { typeOfPoint: req.params.name }})
+        .then(log => res.status(200).json(log))
+        .catch(err => res.status(500).json({ error: err}))
+})
+
+router.delete('/:id', validateSession, (req, res) => {
+    Log.destroy({ where: { id: req.params.id } })
+        .then(result => res.status(200).json(result))
+        .catch(err => res.status(500).json({ error: err }))
+})
+
+module.exports = router;
