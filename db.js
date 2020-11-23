2e16e26b620da2bb493f51e5764b9ebb18e9b130
Adolfo Sandoval <adolfosandoval@Adolfos-MacBook-Pro.local>
2020-11-23 17:29:15 -0500

    env added

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 
 
 //create a new instance of Sequelize, connecting us to a database
-const database = new Sequelize(process.env.NAME, 'postgres', process.env.PASS, {
+const database = new Sequelize(process.env.DATABASE_URL, {
     // host: 'localhost',
     dialect: 'postgres'
 });
